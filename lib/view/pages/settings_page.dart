09cd6fc574419e2cb5faf165a33f4e497034b19b
RB105 <01baxromrajabov@gmail.com>
2023-05-07 00:37:59 +0500

    some features added

--- lib/view/pages/settings_page.dart
+++ lib/view/pages/settings_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:hangomamobile/provider/local/theme_provider.dart';
 import 'package:provider/provider.dart';
 
